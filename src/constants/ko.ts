4a3dbabfa4686956295c412694838cc17d5344bd
Wonjin <fsdjin159@gmail.com>
2021-12-01 18:07:36 +0900

    [WIP]feat: Craete PublicInput

--- src/constants/ko.ts
+++ src/constants/ko.ts
@@ -1,3 +1,14 @@
 export const STRING = {
-  a: 'text',
+  // password
+  morePass: '비밀번호를 6자 이상 입력해주세요.',
+  lessPass: '비밀번호를 12자 이하로 입력해주세요.',
+  PassIncludeNum: '비밀번호에 숫자를 포함해주세요.',
+  PassIncludeEng: '비밀번호에 대소문자 구분없이 영문을 포함해주세요.',
+  PassIncludeSpec: '비밀번호에 특수문자를 포함해주세요.',
+  notMatchPass: '입력하신 비밀번호가 달라요',
+  // Nickname
+  enableNick: '사용 가능한 닉네임이에요',
+  overlapNick: '현재 이미 다른 사람이 사용중인 닉네임이에요',
+  slangNick:
+    '유저닉네임에 비속어/욕설 들어갔을때: 닉네임에 사용이 제한된 단어입니다. 다른 닉네임을 입력해주세요.',
 };
